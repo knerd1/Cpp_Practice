7ce4ee428f61ffd4ef34ed5f197e8c5234d691df
Ashutosh <ap9696446@gmail.com>
2024-10-16 00:20:42 +0530

    First commit

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int score;              //variable declaration
+    score = 685;            //variable initialization
+
+    int totalShows= 22;       //Both declaration and initialization in one line
+
+    const int uid = 8877556;     // we cant change this variable in anywhere
+    //uid = 9545;
+
+    score = 878;
+
+    cout << "hello gon form hXh" << endl;
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;           // We can call any method from namespace from this
+
+using std::cout;               // We can call any method from namespace from this
+using std::endl;
+
+int main(){
+     std::cout <<"hello world from knerd" << endl;      // We can call any method from namespace like this
+     cout << "hello word" <<endl;
+     return 6 ;
+}
+
