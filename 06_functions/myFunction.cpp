b5b104a56b2f9ec2920b1ca2a694e920d85db760
Ashutosh <ap9696446@gmail.com>
2024-10-26 20:34:08 +0530

    eleventh commit

--- 06_functions/myFunction.cpp
+++ 06_functions/myFunction.cpp
@@ -6,20 +6,26 @@ int checkTemp (int temperature){
     return temperature;
 }       
 
-void makeChai(){                                                       //  Declaring and definig the function   
-    cout << "Boiling Water, Adding tea leaves......" << endl;
-}
+ void makeChai(){                                                       //  Declaring and definig the function   
+     cout << "Boiling Water, Adding tea leaves......" << endl;
+ }
 
 void serveChai(int cups);                       // Declaring the function 
+
+void serveChai(string teaype = "Green tea"){    //default parameter
+    int cups = 987;                        // formal parameter
+    cout << "serving "<< teaype << endl;
+}
+ 
+void serveChai(int cups ){                                 // Defining the function
+    cout << cups << " cup of chai serving" << endl;
+}
 
 int main(){
    int temp = checkTemp(50);
    cout << temp << endl;
    makeChai();                       // Calling the function
-   serveChai(3);
+   serveChai(68);                  // Actual parameter
     return 0;
 }
 
-void serveChai(int cups){                                 // Defining the function
-    cout << cups << " cup of chai serving" << endl;
-}
