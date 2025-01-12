d9b2ee654bcbd0409f6ad5ba1a0651d3a6fbbb94
Ashutosh <ap9696446@gmail.com>
2025-01-12 17:19:27 +0530

    patterns

--- Data-Structure/patterns.cpp
+++ Data-Structure/patterns.cpp
@@ -48,8 +48,51 @@ void pat5(int n) {
   }
 }
 
+void pat6(int n) {
+  for (int i = 1; i < n; i++) {
+    for (int j = 1; j <= n - i + 1; j++) {
+      cout << j << " ";
+    }
+    cout << endl;
+  }
+}
+
+void pat7(int n) {
+  for (int i = 0; i < n; i++) {
+    // For Space loop
+    for (int j = 0; j < n - i - 1; j++) {
+      cout << " ";
+    }
+    // For star loop
+    for (int j = 0; j < 2 * i + 1; j++) {
+      cout << "*";
+    }
+    // For space loop
+    for (int j = 0; j < n - i - 1; j++) {
+      cout << " ";
+    }
+    cout << endl;
+  }
+}
+
+void pat8(int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i; j++) {
+      cout << " ";
+    }
+    for (int j = 0; j < 2 * n - (2 * i + 1); j++) {
+      cout << "*";
+    }
+    for (int j = 0; j < i; j++) {
+      cout << " ";
+    }
+    cout << endl;
+  }
+}
+
 int main() {
   int n;
   cin >> n;
-  pat5(n);
+  pat7(n);
+  pat8(n);
 }
