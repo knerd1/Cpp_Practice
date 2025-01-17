6c409102fdb369f58dae0aef58421057f8fc585b
Ashutosh <ap9696446@gmail.com>
2025-01-17 23:52:16 +0530

    Patterns

--- Data-Structure/patterns.cpp
+++ Data-Structure/patterns.cpp
@@ -89,10 +89,22 @@ void pat8(int n) {
     cout << endl;
   }
 }
+// For pat9 = combine pat7 and pat8 it will give star diamond pattern
+
+void pat10(int n) {
+  for (int i = 1; i <= 2 * n - 1; i++) {
+    int star = i;
+    if (i > n)
+      star = 2 * n - i;
+    for (int j = 1; j <= star; j++) {
+      cout << "* ";
+    }
+    cout << endl;
+  }
+}
 
 int main() {
   int n;
   cin >> n;
-  pat7(n);
-  pat8(n);
+  pat10(n);
 }
