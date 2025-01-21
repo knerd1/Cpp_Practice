f8c42560b652e25fabd4974f57a82f9f3caef411
Ashutosh <ap9696446@gmail.com>
2025-01-21 19:10:02 +0530

    sum of first N
    1. using parameterized way
    2. using recursive functional way

--- Data-Structure/Recursion/sumOfFirstN.cpp
+++ Data-Structure/Recursion/sumOfFirstN.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+// Using Parameterized way
+
 void sums(int i, int sum) {
   if (i < 1) {
     cout << sum;
@@ -14,4 +16,19 @@ int main() {
   int n;
   cin >> n;
   sums(n, 0);
+}
+
+// Using Recursive Functional way
+
+int sums(int n) {
+  if (n == 0)
+    return 0;
+  return n + sums(n - 1);
+}
+
+int main() {
+  int n;
+  cin >> n;
+  int result = sums(n);
+  cout << result;
 }
