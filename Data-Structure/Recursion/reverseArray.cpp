711dbd425fc8f7781cf8966aeb64ee8fdb5bcab3
Ashutosh <ap9696446@gmail.com>
2025-01-23 21:24:16 +0530

    reverse Array
    1. with one pointer
    2. with two pointer

--- Data-Structure/Recursion/reverseArray.cpp
+++ Data-Structure/Recursion/reverseArray.cpp
@@ -2,21 +2,38 @@
 #include <utility>
 
 using namespace std;
-void rev(int a[], int l, int r) {
-  if (l >= r)
+
+// void rev(int a[], int l, int r) {
+//   if (l >= r)
+//     return;
+//   swap(a[l], a[r]);
+//   rev(a, l + 1, r - 1);
+// }
+//
+// int main() {
+//   int a[5];
+//   for (int i = 0; i < 5; i++) {
+//     cin >> a[i];
+//   }
+//   rev(a, 0, 4);
+//   for (int i = 0; i < 5; i++) {
+//     cout << a[i] << " ";
+//   }
+//   return 0;
+// }
+//
+void reverse(int arr[], int n, int i) {
+  if (i >= n)
     return;
-  swap(a[l], a[r]);
-  rev(a, l + 1, r - 1);
+  swap(arr[i], arr[n - i - 1]);
+  reverse(arr, n, i + 1);
 }
 
 int main() {
-  int a[5];
-  for (int i = 0; i < 5; i++) {
-    cin >> a[i];
+  int arr[5] = {2, 4, 6, 8, 9};
+  int n = sizeof(arr);
+  reverse(arr, n, 0);
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
   }
-  rev(a, 0, 4);
-  for (int i = 0; i < 5; i++) {
-    cout << a[i] << " ";
-  }
-  return 0;
 }
