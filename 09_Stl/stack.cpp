0990132ec3f87826149fb1460c641ba682f64864
Ashutosh <ap9696446@gmail.com>
2025-01-26 22:42:04 +0530

    Stack data-structure

---
+++ 09_Stl/stack.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void stackUses() {
+  stack<int> st;
+  st.push(1);       //{1}
+  st.push(2);       //{2,1}
+  st.push(3);       //{3,2,1}
+  st.push(3);       //{3,3,2,1}
+  st.emplace(11);   //{11,3,3,2,1}
+  cout << st.top(); // Print 11
+
+  st.pop();           // Now st will look like {3,3,2,1}
+  cout << st.top();   // Print 3
+  cout << st.size();  // 4
+  cout << st.empty(); // False
+
+  stack<int> st1, st2;
+  st1.swap(st2);
+}
+
+int main() { stackUses(); }
