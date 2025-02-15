97c81d5688b2fb4ba7806bea1535d2e08503b586
Ashutosh <ap9696446@gmail.com>
2025-02-15 19:34:47 +0530

    Left Rotate Array by K place

--- Data-Structure/Arrays/leftRotateArrayByKplace.cpp
+++ Data-Structure/Arrays/leftRotateArrayByKplace.cpp
@@ -5,25 +5,25 @@ using namespace std;
 
 void leftRotate(int arr[], int n, int k) {
   // Brute-Force Approach...
-  //
-  //  // If Rotation exceeds...
-  //  k = k % n;
-  //
-  //  // Step 1: take First k elements and put into temp...
-  //  int temp[k];
-  //  for (int i = 0; i < k; i++) {
-  //    temp[i] = arr[i];
-  //  }
-  //
-  //  // Step 2: now shift remaining elements...
-  //  for (int i = k; i < n; i++) {
-  //    arr[i - k] = arr[i];
-  //  }
-  //
-  //  // Step 3: put back temp into array...
-  //  for (int i = n - k; i < n; i++) {
-  //    arr[i] = temp[i - (n - k)];
-  //  }
+
+  // If Rotation exceeds...
+  k = k % n;
+
+  // Step 1: take First k elements and put into temp...
+  int temp[k];
+  for (int i = 0; i < k; i++) {
+    temp[i] = arr[i];
+  }
+
+  // Step 2: now shift remaining elements...
+  for (int i = k; i < n; i++) {
+    arr[i - k] = arr[i];
+  }
+
+  // Step 3: put back temp into array...
+  for (int i = n - k; i < n; i++) {
+    arr[i] = temp[i - (n - k)];
+  }
 
   // Optimal Approach...
   reverse(arr, arr + k);
