8dfea5bd2ea4a0b11a1b538281a656db0e36ae94
Ashutosh <ap9696446@gmail.com>
2025-03-08 18:47:51 +0530

    Next Permutation

--- Data-Structure/Arrays/31-nextPermutation.cpp
+++ Data-Structure/Arrays/31-nextPermutation.cpp
@@ -5,7 +5,8 @@
 
 using namespace std;
 
-vector<int> nums = {1, 2, 3};
+vector<int> nums = {1, 1, 5};
+// Better...
 
 int main() {
   next_permutation(nums.begin(), nums.end());
@@ -13,29 +14,34 @@ int main() {
     cout << it << endl;
   }
 }
+
 // Optimal...
+
 int ind = -1;
 int n = nums.size();
 int main() {
-  for (int i = n - 2; i < 0; i--) {
+  for (int i = n - 2; i >= 0; i--) { // start from back and find smallest dip
+                                     // num
     if (nums[i] < nums[i + 1]) {
       ind = i;
       break;
     }
   }
-  if (ind == -1) {
+
+  if (ind == -1) { // Here if no dip found that means simple reverse the array
     reverse(nums.begin(), nums.end());
-    for (int i = 0; i < n; i++) {
-      cout << nums[i] << endl;
-    }
+    return 0;
   }
-  for (int i = n - 1; i < ind; i--) {
+
+  for (int i = n - 1; i > ind;
+       i--) { // Now find the bigger than ind and (smallest than remaings)
     if (nums[i] > nums[ind]) {
-      swap(nums[i], nums[ind]);
+      swap(nums[i], nums[ind]); // Swap both(1,3...)
       break;
     }
   }
-  reverse(nums.begin() + ind + 1, nums.end());
+
+  reverse(nums.begin() + ind + 1, nums.end()); // At end reverse the left array
   for (auto it : nums) {
     cout << it << endl;
   }
