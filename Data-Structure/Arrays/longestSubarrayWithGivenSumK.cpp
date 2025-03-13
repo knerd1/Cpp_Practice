a0a013d84b808f8790b66548e8c53068755f9b73
Ashutosh <ap9696446@gmail.com>
2025-03-13 23:38:30 +0530

    Revision

--- Data-Structure/Arrays/longestSubarrayWithGivenSumK.cpp
+++ Data-Structure/Arrays/longestSubarrayWithGivenSumK.cpp
@@ -1,88 +1,85 @@
 #include <algorithm>
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
 // Brute Force...
-int getLongestSubarray(vector<int> vt, int K) {
-  int n = vt.size();
+// int getLongestSubarray(vector<int> vt, int sum) {
+//   int n = vt.size();
+//   int length = 0;
+//   for (int i = 0; i < n; i++) {
+//     int sum = 0;
+//     for (int j = i; j < n; j++) {
+//       sum += vt[j];
+//       if (sum == j) {
+//         length = max(length, j - i + 1);
+//       }
+//     }
+//   }
+//   return length;
+// }
+//
+// int main() {
+//   vector<int> vt = {1, 2, 3, 1, 1, 1, 1, 3, 3};
+//   long long sum = 6;
+//   int len = getLongestSubarray(vt, sum);
+//   cout << len << endl;
+// }
 
-  int length = 0;
-  for (int i = 0; i < n; i++) {
-    for (int j = i; j < n; j++) {
-      long long s = 0;
-      for (int k = i; k < j; k++) {
-        s += vt[k];
-      }
-      if (s == K)
-        length = max(length, j - i + 1);
-    }
-  }
-  return length;
-}
-
-int main() {
-  vector<int> vt = {1, 2, 3, 1, 1, 1, 1, 3, 3};
-  long long K = 6;
-  int len = getLongestSubarray(vt, K);
-  cout << len << endl;
-}
-
-// Better approach...
+// // Better approach...
 vector<int> a = {1, 2, 3, 1, 1, 1, 1, 3, 3};
 long long k = 6;
 
 int main() {
-  map<long long, int> preSum;
-  long long sum = 0;
-  int maxLen = 0;
+  unordered_map<long long, int> presum;
+  int sum = 0, length = 0;
+
   for (int i = 0; i < a.size(); i++) {
     sum += a[i];
+
     if (sum == k) {
-      maxLen = max(maxLen, i + 1);
+      length = max(length, i + 1);
     }
+
     long long rem = sum - k;
-    if (preSum.find(rem) != preSum.end()) {
-      int len = i - preSum[rem];
-      maxLen = max(maxLen, len);
+    if (presum.find(rem) != presum.end()) {
+      int len = i - presum[rem];
+      length = max(length, len);
     }
-    if (preSum.find(sum) == preSum.end()) {
-      preSum[sum] = i;
+    if (presum.find(sum) == presum.end()) {
+      presum[sum] = i;
     }
   }
-  cout << maxLen;
-  for (auto it : preSum) {
-    cout << it.first << endl;
-  }
+  cout << length;
 }
 
 // Optimal...
-int longestSubArrayWithSumK(vector<int> a, long long k) {
-  int left = 0, right = 0;
-  long long sum = 0;
-  int maxLen = 0;
-  int n = a.size();
-  while (right < n) {
-    sum += a[right];
-
-    while (left <= right && sum > k) {
-      sum -= a[left];
-      left++;
-    }
-
-    if (sum == k) {
-      maxLen = max(maxLen, right - left + 1);
-    }
-    right++;
-  }
-  return maxLen;
-}
-
-int main() {
-  vector<int> a = {1, 2, 3, 1, 1, 1, 1, 3, 3};
-  long long k = 6;
-  int result = longestSubArrayWithSumK(a, k);
-  cout << result << endl;
-}
+// int longestSubArrayWithSumK(vector<int> a, long long k) {
+//   int left = 0, right = 0;
+//   long long sum = 0;
+//   int maxLen = 0;
+//   int n = a.size();
+//   while (right < n) {
+//     sum += a[right];
+//
+//     while (left <= right && sum > k) {
+//       sum -= a[left];
+//       left++;
+//     }
+//
+//     if (sum == k) {
+//       maxLen = max(maxLen, right - left + 1);
+//     }
+//     right++;
+//   }
+//   return maxLen;
+// }
+//
+// int main() {
+//   vector<int> a = {1, 2, 3, 1, 1, 1, 1, 3, 3};
+//   long long k = 6;
+//   int result = longestSubArrayWithSumK(a, k);
+//   cout << result << endl;
+// }
--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 
 [![wakatime](https://wakatime.com/badge/github/knerd1/Cpp_Practice.svg)](https://wakatime.com/badge/github/knerd1/Cpp_Practice)
 
-[![wakatime](https://wakatime.com/badge/user/b7c1761c-b2c7-44fb-9a02-9811f8b07065/project/754fbe3f-bc49-4111-84bf-2c1a2941e852.svg)](https://wakatime.com/badge/user/b7c1761c-b2c7-44fb-9a02-9811f8b07065/project/754fbe3f-bc49-4111-84bf-2c1a2941e852)
