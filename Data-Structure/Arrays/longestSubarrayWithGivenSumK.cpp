e02e670ff6627215ae310cbc3952ee6684f68231
Ashutosh <ap9696446@gmail.com>
2025-03-14 00:22:34 +0530

    revision

--- Data-Structure/Arrays/longestSubarrayWithGivenSumK.cpp
+++ Data-Structure/Arrays/longestSubarrayWithGivenSumK.cpp
@@ -6,29 +6,29 @@
 using namespace std;
 
 // Brute Force...
-// int getLongestSubarray(vector<int> vt, int sum) {
-//   int n = vt.size();
-//   int length = 0;
-//   for (int i = 0; i < n; i++) {
-//     int sum = 0;
-//     for (int j = i; j < n; j++) {
-//       sum += vt[j];
-//       if (sum == j) {
-//         length = max(length, j - i + 1);
-//       }
-//     }
-//   }
-//   return length;
-// }
-//
-// int main() {
-//   vector<int> vt = {1, 2, 3, 1, 1, 1, 1, 3, 3};
-//   long long sum = 6;
-//   int len = getLongestSubarray(vt, sum);
-//   cout << len << endl;
-// }
+int getLongestSubarray(vector<int> vt, int sum) {
+  int n = vt.size();
+  int length = 0;
+  for (int i = 0; i < n; i++) {
+    int sum = 0;
+    for (int j = i; j < n; j++) {
+      sum += vt[j];
+      if (sum == j) {
+        length = max(length, j - i + 1);
+      }
+    }
+  }
+  return length;
+}
 
-// // Better approach...
+int main() {
+  vector<int> vt = {1, 2, 3, 1, 1, 1, 1, 3, 3};
+  long long sum = 6;
+  int len = getLongestSubarray(vt, sum);
+  cout << len << endl;
+}
+
+// Better approach...   this is Optimal for positives and negatives array
 vector<int> a = {1, 2, 3, 1, 1, 1, 1, 3, 3};
 long long k = 6;
 
@@ -55,31 +55,31 @@ int main() {
   cout << length;
 }
 
-// Optimal...
-// int longestSubArrayWithSumK(vector<int> a, long long k) {
-//   int left = 0, right = 0;
-//   long long sum = 0;
-//   int maxLen = 0;
-//   int n = a.size();
-//   while (right < n) {
-//     sum += a[right];
-//
-//     while (left <= right && sum > k) {
-//       sum -= a[left];
-//       left++;
-//     }
-//
-//     if (sum == k) {
-//       maxLen = max(maxLen, right - left + 1);
-//     }
-//     right++;
-//   }
-//   return maxLen;
-// }
-//
-// int main() {
-//   vector<int> a = {1, 2, 3, 1, 1, 1, 1, 3, 3};
-//   long long k = 6;
-//   int result = longestSubArrayWithSumK(a, k);
-//   cout << result << endl;
-// }
+// Optimal...   only for if array contains (positives and zeros)
+int longestSubArrayWithSumK(vector<int> a, long long k) {
+  int left = 0, right = 0;
+  long long sum = 0;
+  int maxLen = 0;
+  int n = a.size();
+  while (right < n) {
+    sum += a[right];
+
+    while (left <= right && sum > k) {
+      sum -= a[left];
+      left++;
+    }
+
+    if (sum == k) {
+      maxLen = max(maxLen, right - left + 1);
+    }
+    right++;
+  }
+  return maxLen;
+}
+
+int main() {
+  vector<int> a = {1, 2, 3, 1, 1, 1, 1, 3, 3};
+  long long k = 6;
+  int result = longestSubArrayWithSumK(a, k);
+  cout << result << endl;
+}
