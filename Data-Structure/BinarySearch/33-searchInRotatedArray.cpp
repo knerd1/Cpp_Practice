d84e991cd90be9224e7dc30bced0b8c978c8707d
Ashutosh <ap9696446@gmail.com>
2025-03-30 22:41:04 +0530

    Search in Rotated Sorted Array

---
+++ Data-Structure/BinarySearch/33-searchInRotatedArray.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> nums = {7, 8, 9, 1, 2, 3, 4, 5, 6};
+int target = 1;
+int n = nums.size();
+
+int roatedSearch(vector<int> nums, int target, int n) {
+  int low = 0, high = n - 1;
+
+  while (low <= high) {
+    int mid = (low + high) / 2;
+    if (nums[mid] == target)
+      return mid;
+
+    // now Check sorted half
+
+    // Left Half
+    if (nums[low] <= nums[mid]) {
+      if (nums[low] <= target && target <= nums[mid]) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+
+    else { // right half
+      if (nums[mid] <= target && target <= nums[high]) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+  }
+  return -1;
+}
+
+int main(){
+  int result = roatedSearch(nums,  target,n);
+  cout << result << endl;
+}
